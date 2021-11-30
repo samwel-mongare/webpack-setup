de296701d438aa78ac9a8c6c484754d07ec7b85f
Mong'are <Mongaresams@gmail.com>
2021-11-30 14:51:30 +0300

    Create a bundle

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-function component() {
-    const element = document.createElement('div');
+import _ from 'lodash';
 
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+ function component() {
+   const element = document.createElement('div');
 
-    return element;
-}
+  // Lodash, now imported by this script
+   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-document.body.appendChild(component());
+   return element;
+ }
+
+ document.body.appendChild(component());
